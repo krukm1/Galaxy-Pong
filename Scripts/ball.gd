4ec3a68808629273661f1b4311a20e81dd395378
Mateusz <matt130pl@gmail.com>
2025-07-19 09:49:45 -0500

    removed health stuff for now

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,19 +1,18 @@
 extends RigidBody2D
 
-@export var velocity:= Vector2(0,250) # How fast the ball will move.
+@export var velocity := Vector2(0, -250) # Upward velocity by default
+var is_locked := true
+var paddle: CharacterBody2D = null
 
 func _physics_process(delta: float) -> void:
-	
-	var collision_info = move_and_collide(velocity * delta,false,0.08)
-	if collision_info: 
-		#print("Ball collided")
-		velocity = velocity.bounce(collision_info.get_normal())
-		if  collision_info.get_collider().is_in_group("asteroid"):
-			print("Ball collided with Asteroid")
+	if is_locked and paddle:
+		# Keep ball in front of the paddle
+		global_position = paddle.global_position + (paddle.global_position - paddle.center).normalized() * 20.0
+	else:
+		var collision_info = move_and_collide(velocity * delta, false, 0.08)
+		if collision_info:
 			velocity = velocity.bounce(collision_info.get_normal())
-			collision_info.get_collider().queue_free()
 
-func _on_asteroid_ball_bounce(delta) -> void:
-	var collision_info = move_and_collide(velocity * delta,false,0.15)
-	velocity = velocity.bounce(collision_info.get_normal())
-	print("Ball collided with Asteroid")
+func _input(event: InputEvent) -> void:
+	if is_locked and event.is_action_pressed("ui_accept"): # e.g., Spacebar
+		is_locked = false
