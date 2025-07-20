71b7b8f4f72a14732a354052e3718f98be98a280
Mateusz <matt130pl@gmail.com>
2025-07-20 10:42:44 -0500

    level 2 added, but it needs work

---
+++ Scripts/game_level_2.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+func _ready() -> void:
+	Music_Controller.play_level1_music()
+
+#WORKING ON PAUSE BUTTON
+#func _input(event):
+	#if event.is_action_pressed("ui_cancel"):
+		#Music_Controller.play_exit_esc_button()
+		## Toggle pause
+		#get_tree().paused = not get_tree().paused
+		## Show or hide pause menu depending on state
+		#if get_tree().paused:
+			#show()
+		#else:
+			#hide()
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Music_Controller.play_exit_esc_button()
+		get_tree().change_scene_to_file("res://Scenes/Title_Menu.tscn")
